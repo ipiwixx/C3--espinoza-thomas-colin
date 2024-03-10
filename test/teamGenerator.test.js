86133f659636a1adcf77899d3f381d433454dead
ipiwixx <124789918+ipiwixx@users.noreply.github.com>
2024-03-10 11:28:59 +0100

    3. Génère correctement les phases finales

--- test/teamGenerator.test.js
+++ test/teamGenerator.test.js
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import TeamGenerator from "../src/teamGenerator.js";
 import TournamentGenerator from "../src/tournamentGenerator.js";
 
-  describe("Testing TournamentGenerator", () => {
+  describe("Testing TournamentGenerator functions", () => {
     it("1. Génère correctement les poules", () => {
       const teams = [
         { name: "Équipe 1", players: ["Joueur 1", "Joueur 2", "Joueur 3"] },
@@ -28,5 +28,20 @@ import TournamentGenerator from "../src/tournamentGenerator.js";
       tournament.simulatePoulesMatches();
       expect(tournament.finalStages).to.have.lengthOf(1); // Vérifie si une seule phase finale a été générée
       expect(tournament.finalStages[0]).to.have.lengthOf(2); // Vérifie si toutes les équipes qualifiées sont dans la phase finale
+    });
+
+    it('3. Génère correctement les phases finales', () => {
+      const teams = [
+        { name: 'Équipe 1', players: ['Joueur 1', 'Joueur 2', 'Joueur 3'] },
+        { name: 'Équipe 2', players: ['Joueur 4', 'Joueur 5', 'Joueur 6'] },
+        { name: 'Équipe 3', players: ['Joueur 7', 'Joueur 8', 'Joueur 9'] },
+        { name: 'Équipe 4', players: ['Joueur 10', 'Joueur 11', 'Joueur 12'] }
+      ];
+      const tournament = new TournamentGenerator(teams);
+      tournament.generatePoules();
+      tournament.simulatePoulesMatches();
+      tournament.generateFinalStages();
+      expect(tournament.finalStages).to.have.lengthOf.at.least(1); // Vérifie si au moins une phase finale a été générée
+      expect(tournament.finalStages[tournament.finalStages.length - 1]).to.have.lengthOf(1); // Vérifie si la dernière phase finale a été joué
     });
   });
